6b15c706e958532195c50a009fc5639c42598b2c
Joachim Toft <jtoft@kth.se>
2017-01-03 18:44:03 +0100

    Adds horribly inefficient solution for day 11 part one.

--- day11/day11v2.py
+++ day11/day11v2.py
@@ -1,6 +1,8 @@
 import re
 import copy
 import itertools
+from heapq import *
+
 
 def is_even(number):
     return number % 2 == 0
@@ -24,50 +26,56 @@ def get_possible_floors(floor1, floor2):
 
     for pos in p:
         if type(pos) is int:
-            floor2.append(pos)
+            floor2.add(pos)
             floor1.remove(pos)
             if is_legal(floor2) and is_legal(floor1):
                 result.append((copy.copy(floor1), copy.copy(floor2)))
             floor2.remove(pos)
-            floor1.append(pos)
+            floor1.add(pos)
         else:
             for i in pos:
-                floor2.append(i)
+                floor2.add(i)
                 floor1.remove(i)
             if is_legal(floor2) and is_legal(floor1):
                 result.append((copy.copy(floor1), copy.copy(floor2)))
             for i in pos:
                 floor2.remove(i)
-                floor1.append(i)
+                floor1.add(i)
     return result
 
 
 def finished(state):
-    for idx in range(3):
-        if state[1][idx]:
-            return False
-    return True
+    pass
+
+
+def evaluate_state(state):
+    result = 0
+    for idx, floor in enumerate(state['floors']):
+        result += (idx ** 2) * len(floor)
+
+    result -= state['steps'] * 2
+    return -result
 
 
 def get_possible_moves(state):
-    tmp_state = copy.deepcopy(state[1])
+    tmp_state = copy.deepcopy(state['floors'])
     result_states = []
-    el = state[0]['elevator']
+    el = state['elevator']
     if el < 3:
         for pf in get_possible_floors(tmp_state[el], tmp_state[el + 1]):
             new_state = copy.deepcopy(state)
-            new_state[1][el] = pf[0]
-            new_state[1][el + 1] = pf[1]
-            new_state[0]['elevator'] = el + 1
-            new_state[0]['steps'] += 1
+            new_state['floors'][el] = pf[0]
+            new_state['floors'][el + 1] = pf[1]
+            new_state['elevator'] = el + 1
+            new_state['steps'] += 1
             result_states.append(new_state)
     if el > 0:
         for pf in get_possible_floors(tmp_state[el], tmp_state[el - 1]):
             new_state = copy.deepcopy(state)
-            new_state[1][el] = pf[0]
-            new_state[1][el - 1] = pf[1]
-            new_state[0]['elevator'] = el - 1
-            new_state[0]['steps'] += 1
+            new_state['floors'][el] = pf[0]
+            new_state['floors'][el - 1] = pf[1]
+            new_state['elevator'] = el - 1
+            new_state['steps'] += 1
             result_states.append(new_state)
     return result_states
 
@@ -83,41 +91,53 @@ def is_legal(floor):
 match_gen = re.compile(r'\w+(?= generator)')
 match_chip = re.compile(r'\w+(?=-compatible)')
 elements = {}
-floors = [[] for _ in range(4)]
+floors = [set() for _ in range(4)]
 elevator = 0
 count = 0
 
 with open('input.txt', 'r') as f:
+#with open('input_part2.txt', 'r') as f:
     for i, line in enumerate(f):
 
         for m in re.finditer(match_chip, line):
             if not elements.has_key(m.group(0)):
                 elements[m.group(0)] = count * 2
                 count += 1
-            floors[i].append(elements[m.group(0)])
+            floors[i].add(elements[m.group(0)])
 
         for m in re.finditer(match_gen, line):
             if not elements.has_key(m.group(0)):
                 elements[m.group(0)] = count * 2
                 count += 1
-            floors[i].append(elements[m.group(0)] + 1)
+            floors[i].add(elements[m.group(0)] + 1)
 
-st = [{'elevator': 0, 'steps': 0}, floors]
+st = {'elevator': 0, 'steps': 0, 'floors': floors}
 queue = [st]
-visited = []
+# heappush(queue, (evaluate_state(st), st))
+visited = set()
 
 while queue:
-    print queue[0], len(queue), len(visited)
+    # pop = heappop(queue)
+    # cur = pop[1]
 
-    if finished(queue[0]):
-        print queue[0]
+    cur = queue[0]
+
+    print cur, len(queue), len(visited)  # , 'eval: ', -pop[0]
+    if len(cur['floors'][3]) == 10:
+        print '\n', cur
+        raw_input('Press enter to continue')
+
+    if finished(cur):
+        print cur
         break
 
-    if queue[0][1] not in visited:
-        for s in get_possible_moves(queue[0]):
-            if s[1] not in visited:
-                queue.append(s)
-                #queue.insert(0, s)
+    floors = [frozenset(f) for f in cur['floors']]
+    if (cur['elevator'], tuple(floors)) not in visited:
+        for pos_state in get_possible_moves(cur):
+            p_floors = [frozenset(f) for f in pos_state['floors']]
+            if (pos_state['elevator'], tuple(p_floors)) not in visited:
+                # heappush(queue, (evaluate_state(pos_state), pos_state))
+                queue.append(pos_state)
 
-    visited.append(queue[0][1])
+    visited.add((cur['elevator'], tuple(floors)))
     del queue[0]
