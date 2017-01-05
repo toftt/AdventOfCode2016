e091bb0530129ba558e06dcb44a4b6a3239d6a86
Joachim Toft <jtoft@kth.se>
2017-01-05 23:04:51 +0100

    Shortens solution for day 13 by 8 lines

--- day13/day13.py
+++ day13/day13.py
@@ -1,10 +1,3 @@
-moves = {
-    'r': lambda x, y: (x + 1, y),
-    'l': lambda x, y: (x - 1, y),
-    'd': lambda x, y: (x, y + 1),
-    'u': lambda x, y: (x, y - 1)
-}
-
 with open('input.txt', 'r') as f:
     fav_number = int(f.read())
 
@@ -18,17 +11,16 @@ marked = [[False for i in range(length)] for j in range(length)]
 queue = [(1, 1)]
 distance[1][1] = 0
 while queue:
-    x, y = queue[0]
+    x, y = queue.pop(0)
     marked[x][y] = True
 
-    for move in moves.itervalues():
-        new_x, new_y = move(x, y)
+    for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        new_x, new_y = map(sum, zip(move, (x, y)))
 
         if (min(new_x, new_y) >= 0 and max(new_x, new_y) < length and
                 not marked[new_x][new_y] and is_open[new_x][new_y]):
             distance[new_x][new_y] = distance[x][y] + 1
             queue.append((new_x, new_y))
-    del queue[0]
 
 number_reachable = sum(1 if 0 <= item <= 50 else 0 for sublist in distance for item in sublist)
 
